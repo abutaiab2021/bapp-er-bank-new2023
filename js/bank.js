6d015c2049f3301d91f0de9be087519a44a57664
abutaiab2021 <abutaiabcse95@gmail.com>
2023-11-26 16:26:19 +0600

    deposit part complete

--- js/bank.js
+++ js/bank.js
@@ -1,1 +1,27 @@
-console.log("Bank.js File Connected");
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  //   console.log("deposit button click");
+  const depositField = document.getElementById("deposit-field");
+  const depositFieldValueString = depositField.value;
+  const depositFieldValueFloat = parseFloat(depositFieldValueString);
+  //   console.log(typeof depositFieldValueFloat);
+  //   console.log(depositFieldValueFloat);
+
+  const previousDeposit = document.getElementById("previous-deposit-text");
+  const previousDepositTextString = previousDeposit.innerText;
+  const previousDepositTextFloat = parseFloat(previousDepositTextString);
+
+  const totalDepositAmount = previousDepositTextFloat + depositFieldValueFloat;
+  //   console.log(typeof previousDepositTextFloat);
+  //   console.log(previousDepositTextFloat);
+  previousDeposit.innerText = totalDepositAmount;
+
+  const preBalanceTotal = document.getElementById("balance-total");
+  const preBalanceTotalString = preBalanceTotal.innerText;
+  const preBalanceTotalFloat = parseFloat(preBalanceTotalString);
+  //   console.log(typeof balanceTotalFloat);
+
+  const currentBalanceTotal = preBalanceTotalFloat + depositFieldValueFloat;
+  preBalanceTotal.innerText = currentBalanceTotal;
+
+  depositField.value = "";
+});
