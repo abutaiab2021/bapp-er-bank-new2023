f4618bb52cc6817446acee76b1bd21042dc65230
abutaiab2021 <abutaiabcse95@gmail.com>
2023-11-28 11:36:52 +0600

    text editor inraction 50% done

--- js/texteditor.js
+++ js/texteditor.js
@@ -1,3 +1,30 @@
 document.getElementById("btn-exit").addEventListener("click", function () {
   window.location = "bank.html";
 });
+
+document.getElementById("btn-bold").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.fontWeight = "bold";
+});
+
+document.getElementById("btn-italic").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.fontStyle = "italic";
+});
+
+document.getElementById("btn-under").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.textDecoration = "underline";
+});
+document.getElementById("text-left").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.textAlign = "left";
+});
+document.getElementById("text-right").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.textAlign = "right";
+});
+document.getElementById("text-center").addEventListener("click", function () {
+  const textArea = document.getElementById("text-area");
+  textArea.style.textAlign = "center";
+});
